853b5e54e7b7af48ad5884d40bae58ce59274774
Murat Serhat Alperen <hi@muratserhatalperen.com>
2024-12-31 00:22:27 +0300

    deneme

--- Runtime/TheSingleton/TheSingletonManager.cs
+++ Runtime/TheSingleton/TheSingletonManager.cs
@@ -9,7 +9,10 @@ namespace ZeroGame
     [InitializeOnLoad]
     internal static class TheSingletonManager
     {
-        private const string ASSET_PATH = "Assets/Runtime/TheSingleton/TheSingleton.prefab";
+        private const string ASSET_PATH_IN_PROJECT = "Assets/ZeroGame/TheSingleton.prefab";
+        private const string ASSET_PATH_IN_PACKAGE = "Packages/ZeroGame/Runtime/TheSingleton/TheSingleton.prefab";
+        private const string ASSET_PATH_PACKAGE_MODE = "Assets/Runtime/TheSingleton/TheSingleton.prefab";
+
         private static TheSingleton theSingletonInstance;
 
 
@@ -38,10 +41,30 @@ namespace ZeroGame
         /// <returns></returns>
         internal static TheSingleton GetAsset()
         {
-            var asset = AssetDatabase.LoadAssetAtPath<TheSingleton>(ASSET_PATH);
-            if(asset == null)
+            // Try loading from the project folder first
+            var asset = AssetDatabase.LoadAssetAtPath<TheSingleton>(ASSET_PATH_IN_PROJECT);
+
+            if (asset == null)
             {
-                Debug.LogError("HEREE!");
+                // If asset isn't found, we are likely in package mode, try loading from the package folder
+                asset = AssetDatabase.LoadAssetAtPath<TheSingleton>(ASSET_PATH_PACKAGE_MODE);
+
+                if (asset == null)
+                {
+                    // If asset still not found, copy it from the package to the Assets folder
+                    bool result = AssetDatabase.CopyAsset(ASSET_PATH_IN_PACKAGE, ASSET_PATH_IN_PROJECT);
+
+                    if (result)
+                    {
+                        // Refresh the asset database and load the asset from the project folder
+                        AssetDatabase.Refresh();
+                        asset = AssetDatabase.LoadAssetAtPath<TheSingleton>(ASSET_PATH_IN_PROJECT);
+                    }
+                    else
+                    {
+                        Debug.LogError("TheSingleton prefab not found in the package. Please ensure the prefab is located in the package at 'Packages' + ASSET_PATH.");
+                    }
+                }
             }
             return asset;
         }
