fce110879a3554dff695f6288c4f33b00ec50289
Murat Serhat Alperen <muratalperen@protonmail.com>
2025-10-02 16:25:15 +0300

    Delayer

--- Runtime/Scripts/UsefulClasses/Delayer.cs
+++ Runtime/Scripts/UsefulClasses/Delayer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using UnityEngine;
+
 public class Delayer
 {
     public static void Delay(Action action, float delay)
@@ -9,6 +10,29 @@ public class Delayer
         IEnumerator DelayCR()
         {
             yield return new WaitForSeconds(delay);
+            action.Invoke();
+        }
+    }
+
+    public static void Delay(float delay, Action action) => Delay(action, delay);
+
+
+    public static void DelayUntil(Func<bool> condition, Action action)
+    {
+        TheSingleton.Instance.StartCoroutine(DelayUntilCR());
+        IEnumerator DelayUntilCR()
+        {
+            yield return new WaitUntil(condition);
+            action.Invoke();
+        }
+    }
+
+    public static void DelayWhile(Func<bool> condition, Action action)
+    {
+        TheSingleton.Instance.StartCoroutine(DelayWhileCR());
+        IEnumerator DelayWhileCR()
+        {
+            yield return new WaitWhile(condition);
             action.Invoke();
         }
     }
