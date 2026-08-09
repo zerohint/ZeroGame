efef098e7630c8db4c53cdd4e8f7bae0137887f6
Murat Serhat Alperen <muratalperen@protonmail.com>
2026-08-09 13:31:51 +0300

    Panelmanager dontdestroy on load

--- Runtime/UI/Panel/PanelManager.cs
+++ Runtime/UI/Panel/PanelManager.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
 
+
+/// <summary>
+/// Scene independent panels
+/// </summary>
 [CreateAssetMenu(menuName = "Game/Managers/Panel Manager")]
-public class PanelManager : SingletonSC<PanelManager>
+public partial class PanelManager : SingletonSC<PanelManager>
 {
     /// <summary>
     /// A root panel is stacked, home in the menu scene
@@ -102,7 +104,10 @@ public class PanelManager : SingletonSC<PanelManager>
         if (!panelData.IsInstanced)
         {
             if (!runtimeCanvas.IsExists())
+            {
                 runtimeCanvas = Instantiate(canvasPrefab);
+                DontDestroyOnLoad(runtimeCanvas);
+            }
             Loadings.Instance.Show(runtimeCanvas.transform);
             await panelData.Load(runtimeCanvas.transform);
             Loadings.Instance.Hide(runtimeCanvas.transform);
@@ -227,132 +232,5 @@ public class PanelManager : SingletonSC<PanelManager>
         if (panels == null) return;
         foreach (var panel in panels)
             panel.OnValidate();
-    }
-
-
-
-
-    [Serializable]
-    private class PanelData
-    {
-        /// <summary>
-        /// Dictionary key, baked from the prefab on validate.
-        /// Kept as a name because Unity can't serialize <see cref="Type"/>
-        /// </summary>
-        public Type PanelType => panelType ??= Type.GetType(panelTypeName ?? string.Empty);
-        [SerializeField, HideInInspector] private string panelTypeName;
-        [NonSerialized] private Type panelType;
-
-        [SerializeField] private AssetReferenceGameObject panelAddressable;
-
-        [NonSerialized] public Panel instanced;
-        [NonSerialized] private AsyncOperationHandle<GameObject> asyncHandle;
-        [NonSerialized] private Task<Panel> loadTask;
-
-        public bool IsInstanced => instanced.IsExists();
-
-
-        /// <summary>
-        /// Load addressable and instantiate it.
-        /// Concurrent calls share one task instead of instantiating twice
-        /// </summary>
-        /// <param name="parent"></param>
-        /// <returns></returns>
-        public Task<Panel> Load(Transform parent)
-        {
-            if (IsInstanced)
-                return Task.FromResult(instanced);
-
-            return loadTask ??= LoadInternal(parent);
-        }
-
-
-        /// <exception cref="Exception"></exception>
-        private async Task<Panel> LoadInternal(Transform parent)
-        {
-            try
-            {
-                if (!asyncHandle.IsValid())
-                    asyncHandle = panelAddressable.LoadAssetAsync();
-                await asyncHandle.Task;
-
-                if (asyncHandle.Status != AsyncOperationStatus.Succeeded)
-                {
-                    Debug.LogError($"[PanelData.Load] Failed to load panel: " + ToString());
-                    Release();
-                    return null;
-                }
-
-                var go = UnityEngine.Object.Instantiate(asyncHandle.Result, parent);
-                if (!go.TryGetComponent(out instanced))
-                {
-                    UnityEngine.Object.Destroy(go);
-                    throw new Exception($"[PanelData.Load] No {nameof(Panel)} component on prefab: " + ToString());
-                }
-                return instanced;
-            }
-            finally
-            {
-                loadTask = null;
-            }
-        }
-
-
-        /// <summary>
-        /// On game being destroyed
-        /// In editor, game closed. I'm not sure needed on build
-        /// </summary>
-        public void OnDestroy() => Release();
-
-
-        /// <summary>
-        /// Back to the never-loaded state.
-        /// Matters on a ScriptableObject, it outlives play mode in the editor
-        /// </summary>
-        private void Release()
-        {
-            if (instanced.IsExists())
-                UnityEngine.Object.Destroy(instanced.gameObject);
-            instanced = null;
-            loadTask = null;
-
-            if (asyncHandle.IsValid())
-                panelAddressable.ReleaseAsset();
-            asyncHandle = default;
-        }
-
-
-        public void OnValidate()
-        {
-#if UNITY_EDITOR
-            panelTypeName = null;
-            panelType = null;
-
-            if (panelAddressable == null || !panelAddressable.RuntimeKeyIsValid())
-                return;
-
-            var path = UnityEditor.AssetDatabase.GUIDToAssetPath(panelAddressable.AssetGUID);
-            var prefab = UnityEditor.AssetDatabase.LoadAssetAtPath<GameObject>(path);
-            if (prefab == null || !prefab.TryGetComponent<Panel>(out var panel))
-            {
-                Debug.LogError($"[PanelData.OnValidate] No {nameof(Panel)} component on: {path}");
-                return;
-            }
-
-            panelTypeName = panel.GetType().AssemblyQualifiedName;
-#endif
-        }
-
-
-        public override string ToString()
-        {
-            return $"{nameof(PanelData)}({panelTypeName ?? "unbaked"})";
-        }
-
-        // [Serializable]
-        // public class AssetReferencePanel : AssetReferenceT<Panel>
-        // {
-        //     public AssetReferencePanel(string guid) : base(guid) { }
-        // }
     }
 }
